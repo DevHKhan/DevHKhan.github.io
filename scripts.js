2b7839e82ea00accf8d986e9bad23d302d59d585
DevHKhan <vincentwufortnite@gmail.com>
2025-03-15 18:54:42 -0500

    Add another example for diacritic practice

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
-function diacritic_answer () {
+function diacritic_answer1 () {
     setTimeout(() => {
         document.getElementById('diacritic_q1').scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }, 300);
+}
+
+function diacritic_answer2 () {
+    setTimeout(() => {
+        document.getElementById('diacritic_q2').scrollIntoView({ behavior: 'smooth', block: 'start' });
     }, 300);
 }
