eed99764f10c0acc0aede0c8faa8fe2153b82e5c
DevHKhan <habib_khan09@outlook.com>
2025-07-07 14:16:29 -0500

    Add scroll progress bar

--- scripts.js
+++ scripts.js
@@ -9,3 +9,12 @@ function diacritic_answer2 () {
         document.getElementById('diacritic_q2').scrollIntoView({ behavior: 'smooth', block: 'start' });
     }, 300);
 }
+
+function getScrollProgress() {
+    let percentProgress = (scrollY / pageHeight) * 100;
+    scrollProgress.style.width = percentProgress + '%';
+}
+
+let scrollProgress = document.getElementById('scroll_progress');
+let pageHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+window.onscroll = getScrollProgress;
