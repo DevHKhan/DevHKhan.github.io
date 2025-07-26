5aea6605de8ad4caffb48c0fb8b8124c795e86c3
DevHKhan <habib_khan09@outlook.com>
2025-07-26 17:01:07 -0500

    Add router links

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,6 @@ import PashtoCard from './assets/pashto_card.png'
 <template>
   <div id="app">
     <Header />
-
-    <div class="container-fluid">
-      <h1>Home</h1>
-
-      <div class="card w-25 mx-auto mt-5">
-        <img :src="PashtoCard" class="card-img-top" alt="Pashto">
-        <div class="card-body">
-          <h5 class="card-title">Learn Pashto</h5>
-          <p class="card-text">Join me on a journey to learn the beautiful language of Pashto!!</p>
-          <router-link to="/pashto/" class="btn btn-primary">Let's Go!</router-link>
-        </div>
-      </div>
-            
-    </div>
-
     <Footer />
   </div>
 </template>
