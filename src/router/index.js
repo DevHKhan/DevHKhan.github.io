f62d4dd0b244225907a822e25a45157215ac068f
DevHKhan <habib_khan09@outlook.com>
2025-07-26 17:41:06 -0500

    Update router links

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Pashto from "../views/Pashto.vue";
 import Lessons from "../views/Lessons.vue";
+import Alphabet from "../views/Alphabet.vue";
 
 const routes = [
   { path: "/", component: Home },
