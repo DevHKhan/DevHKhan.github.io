d4d799f9e286c1cc447e6bf187f4d2d8d9794969
DevHKhan <habib_khan09@outlook.com>
2025-07-26 16:29:54 -0500

    Update source before deploy

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ import PashtoCard from './assets/pashto_card.png'
         <img :src="PashtoCard" class="card-img-top" alt="Pashto">
         <div class="card-body">
           <h5 class="card-title">Learn Pashto</h5>
-          <p class="card-text">Join me on a journey to learn the beautiful language of Pashto!</p>
+          <p class="card-text">Join me on a journey to learn the beautiful language of Pashto!!</p>
           <a href="/pashto/" class="btn btn-primary">Let's Go!</a>
         </div>
       </div>
