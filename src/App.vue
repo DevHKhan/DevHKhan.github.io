a6c652253b151a8fc4a57fed0868045a53d88aae
DevHKhan <habib_khan09@outlook.com>
2025-07-26 16:23:30 -0500

    Migrate to Vue.js

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import PashtoCard from './assets/pashto_card.png'
+</script>
+
+<template>
+  <div id="app">
+    <Header />
+
+    <div class="container-fluid">
+      <h1>Home</h1>
+
+      <div class="card w-25 mx-auto mt-5">
+        <img :src="PashtoCard" class="card-img-top" alt="Pashto">
+        <div class="card-body">
+          <h5 class="card-title">Learn Pashto</h5>
+          <p class="card-text">Join me on a journey to learn the beautiful language of Pashto!</p>
+          <a href="/pashto/" class="btn btn-primary">Let's Go!</a>
+        </div>
+      </div>
+            
+    </div>
+
+    <Footer />
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="sticky-top">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/"><i class="fa-solid fa-house me-2"></i>Home</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pashto/">Pashto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pashto/lessons/"><i class="fa-solid fa-book me-2"></i>Lessons</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div id="scroll_progress"></div>
+
+    </div>
+</template>
