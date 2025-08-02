6c8876f618ab66425199b6b53cde167d31220f40
DevHKhan <habib_khan09@outlook.com>
2025-08-02 15:41:46 -0500

    Make home page more interesting

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="p-3 bg-dark text-white text-center">
-    <p>Habib Khan</p>
+    <p>&copy; Habib Khan</p>
     <p class="text-pashto">حبیب خان</p>
   </footer>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@ import PashtoCalligraphy from "../assets/pashto_calligraphy.png";
 </script>
 
 <template>
-  <div class="container-fluid p-0 fw-bold">
+  <div class="container-fluid p-0">
     <div class="pt-5">
       <img
         :src="PashtoCalligraphy"
@@ -12,19 +12,34 @@ import PashtoCalligraphy from "../assets/pashto_calligraphy.png";
       />
     </div>
 
-    <h1 class="display-1 text-center my-5">Welcome</h1>
+    <h1 class="display-1 my-5 fw-bold">Welcome</h1>
     <div class="col-md-6 mx-auto">
-      <p class="lead text-center">
+      <p class="lead text-center fw-bold">
         This is my website where you can learn Pashto. I have created numerous
         lessons to help you on your journey toward mastering this truly
         beautiful language. Enjoy!
       </p>
+      <div
+        class="d-flex flex-column flex-md-row justify-content-center gap-4 m-5"
+      >
+        <a href="/#/pashto/" class="btn btn-lg flex-fill" role="button"
+          ><span class="text-white">Pashto</span></a
+        >
+        <a href="/#/lessons/" class="btn btn-lg flex-fill" role="button"
+          ><span class="text-white"
+            ><i class="fa-solid fa-book me-2"></i>Lessons</span
+          ></a
+        >
+        <a href="/#/dictionary/" class="btn btn-lg flex-fill" role="button"
+          ><span class="text-white">Dictionary</span></a
+        >
+      </div>
     </div>
 
     <div class="bg-first-color text-white p-3">
       <div class="col-md-6 mx-auto">
-        <h1 class="display-1 mb-5">Pashto</h1>
-        <p class="lead text-center">
+        <h1 class="display-1 mb-5 fw-bold">Pashto</h1>
+        <p class="lead text-center fw-bold">
           Let me take you on a journey to show you the true beauty of the Pashto
           language.
         </p>
@@ -40,18 +55,64 @@ import PashtoCalligraphy from "../assets/pashto_calligraphy.png";
 
     <div class="bg-second-color text-first-color p-3">
       <div class="col-md-6 mx-auto">
-        <h1 class="display-1 mb-5">Interactive Lessons</h1>
-        <p class="lead text-center">
+        <h1 class="display-1 mb-5 fw-bold">Interactive Lessons</h1>
+        <p class="lead text-center fw-bold">
           Lessons are designed thoughtfully with an interactive approach to
           encourage active learning.
         </p>
+      </div>
+    </div>
+    <div
+      class="text-bg-dark overflow-hidden position-relative text-nowrap w-100"
+    >
+      <div class="marquee-slider">
+        <span class="marquee-text">
+          <h1 class="display-1 d-inline-block fw-bold">
+            Learn | Read | Write | Speak |
+          </h1>
+        </span>
+        <span class="marquee-text">
+          <h1 class="display-1 d-inline-block fw-bold">
+            Learn | Read | Write | Speak |
+          </h1>
+        </span>
+      </div>
+    </div>
+
+    <div class="bg-first-color text-white p-3">
+      <h1 class="display-1 mb-5 fw-bold">Comprehensive Dictionary</h1>
+      <div class="col-md-6 mx-auto">
+        <p class="lead text-center fw-bold">Never miss another word.</p>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+@keyframes marqueeslide {
+  0% {
+    transform: translateX(0);
+  }
+
+  100% {
+    transform: translateX(-50%);
+  }
+}
+
 .container-fluid {
   background: linear-gradient(#6f4e37, #f4e3d7);
+}
+
+.marquee-slider {
+  display: inline-block;
+  white-space: nowrap;
+  animation: marqueeslide 20s linear infinite;
+}
+
+.marquee-text {
+  display: inline-block;
+  white-space: nowrap;
+  word-spacing: 25rem;
+  margin-right: 25rem;
 }
 </style>
