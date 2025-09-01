98f98e3cbf8a1b56954db2500ef8ae733b828f29
mehreen.arbab@outlook.com <habib_khan09@outlook.com>
2025-09-01 13:38:45 -0500

    Add proper anchor scrolling

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+      };
+    } else if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
